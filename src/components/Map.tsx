de2eb7c930506d92cabe98743e9403e2c3f93b61
Lucas Xavier <lucasbritesxavier2010@gmail.com>
2025-03-15 21:03:44 -0300

    dynamic icon with icon picker ;)

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,25 +1,31 @@
+import { createElement, useState } from "react";
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import L from "leaflet";
-import { FaMapMarkerAlt } from "react-icons/fa";
-import "leaflet/dist/leaflet.css";
+import IconPicker from "react-icons-picker";
 import { renderToString } from "react-dom/server";
-
-const CustomMarkerIcon = L.divIcon({
-  className: "custom-marker-icon",
-  html: `<div style="font-size: 24px; color: red;">${renderToString(
-    <FaMapMarkerAlt />
-  )}</div>`,
-  iconSize: [30, 30],
-  iconAnchor: [15, 30],
-});
+import "leaflet/dist/leaflet.css";
 
 export default function Map() {
+  const [selectedIcon, setSelectedIcon] = useState("FaUsers");
+
+  const CustomMarkerIcon = L.divIcon({
+    className: "custom-marker-icon",
+    html: `<div style="font-size: 24px; color: red;">${renderToString(
+      createElement(require("react-icons/fa")[selectedIcon])
+    )}</div>`,
+    iconSize: [30, 30],
+    iconAnchor: [15, 30],
+  });
+
   return (
-    <MapContainer center={[-23.55052, -46.633308]} zoom={13} className="w-full h-full">
-      <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
-      <Marker position={[-23.55052, -46.633308]} icon={CustomMarkerIcon}>
-        <Popup>Testing react-icons inside leaflet</Popup>
-      </Marker>
-    </MapContainer>
+    <div className="flex flex-col items-center">
+      <IconPicker value={selectedIcon} onChange={setSelectedIcon} />
+      <MapContainer center={[-23.55052, -46.633308]} zoom={13} className="w-full h-96">
+        <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+        <Marker position={[-23.55052, -46.633308]} icon={CustomMarkerIcon}>
+          <Popup>Dynamic icon!</Popup>
+        </Marker>
+      </MapContainer>
+    </div>
   );
 }
